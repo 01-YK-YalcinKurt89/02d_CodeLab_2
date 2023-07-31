68bc7d19da520287b666c5b6aa33d9aa243baabd
Yalcin Kurt <yalcin.kurt89@gmail.com>
2023-07-31 23:27:41 +0200

    added comments

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,23 +4,30 @@ public class App {
 
 	public static void main(String[] args) {
 	
+
+// Erstellen von drei Mitarbeiter-Objekten und Initialisieren mit Werten
 	Mitarbeiter mitarbeiter1 = new Mitarbeiter("Mütze", "Maxine", "CTO", 2017);
 	Mitarbeiter mitarbeiter2 = new Mitarbeiter("Mützerich", "Max", "COO", 2018);
 	Mitarbeiter mitarbeiter3 = new Mitarbeiter("Martinelli", "Silvia", "CEO", 2020);
 	
+// Aufrufen der Methode "pollMitarbeiter" für jedes Mitarbeiter-Objekt
 	pollMitarbeiter(mitarbeiter1);
 	pollMitarbeiter(mitarbeiter2);
 	pollMitarbeiter(mitarbeiter3);
-
 	}
 
+
+// Methode zur Ausgabe von Mitarbeiter-Informationen
 	private static void pollMitarbeiter(Mitarbeiter ma) {
+	// Die Methode "getInfo" wird aufgerufen, um Mitarbeiter-Informationen abzurufen und auszugeben
 		output(ma.getInfo("#name"));
 		output(ma.getInfo("#position"));
 		output(ma.getInfo("#time"));
 		output("-------------------");
 	} 
 
+
+// Methode zur Ausgabe von Strings
 	private static void output(String outStr) {
 		System.out.println(outStr);
 	}
--- com/cc/java/Mitarbeiter.java
+++ com/cc/java/Mitarbeiter.java
@@ -7,6 +7,8 @@ public class Mitarbeiter {
 	private String role ;
 	private int yearOfEntry ;
 	
+
+// Konstruktor zur Initialisierung der Mitarbeiter-Objekte
 	public Mitarbeiter(String familyName, String firstName, String role, int yearOfEntry) {
 		this.familyName = familyName;
 		this.firstName = firstName;
@@ -15,14 +17,19 @@ public class Mitarbeiter {
 	}
 
 
+// Methode zur Abfrage von Mitarbeiter-Informationen basierend auf dem übergebenen Flag
 	public String getInfo(String flag) {
 		switch (flag) {
+			// Wenn das Flag "#name" ist, gibt die Methode Vorname und Nachname des Mitarbeiters zurück
 			case "#name":
 				return this.firstName +" " + this.familyName;
+            // Wenn das Flag "#position" ist, gibt die Methode die Rolle/Position des Mitarbeiters zurück
 			case "#position":
 				return this.role;
+            // Wenn das Flag "#time" ist, gibt die Methode das Jahr des Eintritts des Mitarbeiters zurück
 			case "#time":
 				return Integer.toString(this.yearOfEntry);
+            // Für jeden anderen Wert des Flags gibt die Methode "ERROR" zurück
 			default:
 			return "ERROR";
 		}
